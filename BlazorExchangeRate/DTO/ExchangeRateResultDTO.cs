7a4f99262774dbfb9a0d2aaebc32f678c4e40cdf
Rafael de Paula <xspynks@gmail.com>
2022-11-15 01:46:21 -0300

    feat: create Task method for ExchangeCurrencyService

--- BlazorExchangeRate/DTO/ExchangeRateResultDTO.cs
+++ BlazorExchangeRate/DTO/ExchangeRateResultDTO.cs
@@ -4,6 +4,10 @@ namespace BlazorExchangeRate.DTO;
 
 public class ExchangeRateResultDTO
 {
+    public ExchangeRateResultDTO()
+    {
+        Rates = new Dictionary<string, double>();
+    }
     [JsonPropertyName("conversion_rates")]
     public Dictionary<string, double> Rates { get; set; }
 }
--- BlazorExchangeRate/Services/ExchangeCurrencyService.cs
+++ BlazorExchangeRate/Services/ExchangeCurrencyService.cs
@@ -1,6 +1,37 @@
+using System.Text.Json;
+using BlazorExchangeRate.Configurations;
+using BlazorExchangeRate.DTO;
+
 namespace BlazorExchangeRate.Services;
 
 public class ExchangeCurrencyService
 {
-    
+    private readonly HttpClient httpCLient;
+    private readonly ExchangeRateApiConfig exchangeRateApiConfig;
+
+    public ExchangeCurrencyService(IHttpClientFactory httpClientFactory, ExchangeRateApiConfig exchangeRateApiConfig)
+    {
+        httpCLient = httpClientFactory.CreateClient("ExchangeCurrencyService");
+        this.exchangeRateApiConfig = exchangeRateApiConfig;
+    }
+
+    public async Task<double> ConvertUsdToBrl(double usdValue)
+    {
+        using (var httpResponse = await httpCLient.GetAsync($"/v6/{exchangeRateApiConfig.ApiKey}/latest/USD"))
+        {
+            if (httpResponse.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var errorResult = await httpResponse.Content.ReadAsStringAsync();
+                throw new Exception(errorResult);
+            }
+
+            var jsonString = await httpResponse.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ExchangeRateResultDTO>(jsonString);
+            var brlValueToOneUsd = result.Rates["BRL"];
+
+            var resultInBrl = Math.Round(usdValue * brlValueToOneUsd, 2);
+
+            return resultInBrl;
+        }
+    }
 }
