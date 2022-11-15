7f4ab00e1eb718fac69b51b8dce7f16e2d6ab944
Rafael de Paula <xspynks@gmail.com>
2022-11-15 01:28:19 -0300

    feat: add Configuration folder and ExchangeRate class, add builder

---
+++ BlazorExchangeRate/Configurations/ExchangeRateApiConfig.cs
@@ -0,0 +1,6 @@
+namespace BlazorExchangeRate.Configurations;
+
+public class ExchangeRateApiConfig
+{
+    public string ApiKey { get; set; }
+}
--- BlazorExchangeRate/Program.cs
+++ BlazorExchangeRate/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorExchangeRate;
+using BlazorExchangeRate.Configurations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var exchangeRateApiKey = builder.Configuration["ExchangeRateApiConfig:ApiKey"];
+
+builder.Services.AddSingleton(new ExchangeRateApiConfig() { ApiKey = exchangeRateApiKey });
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
